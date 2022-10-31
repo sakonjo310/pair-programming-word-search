94bc67da23fb97260af033067c0f8e7db7b74c96
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-31 01:47:16 -0700

    Add function to check words diagonally

--- wordsearch.js
+++ wordsearch.js
@@ -4,11 +4,13 @@ const wordSearch = (letters, word) => {
     if (letters.length > 0) {
         let reverseWord = word.split('').reverse().join('');
         const horizontalJoin = letters.map(ls => ls.join(''))
+// Check horiontally
         for (let l of horizontalJoin) {
             if (l.includes(word) || l.includes(reverseWord)) {
                 return true;
             }
         }
+// Check vertically
         let verticalArrs = [];
         for (let i = 0; i < letters[0].length; i++) {
             let verticalArr = [];
@@ -19,8 +21,48 @@ const wordSearch = (letters, word) => {
         }
         verticalJoin = verticalArrs.map(vls => vls.join(''));
 
-        for (let arr of verticalJoin) {
-            if (arr.includes(word) || arr.includes(reverseWord)) {
+        for (let v of verticalJoin) {
+            if (v.includes(word) || v.includes(reverseWord)) {
+                return true;
+            }
+        }
+
+// Check diagonally
+        const rows = horizontalJoin.length;
+        const cols = horizontalJoin[0].length;
+        let diagArr = [];
+        let diagArr2 = [];
+
+        for (let n = 0; n < cols + rows - 1; n++) {
+            let r = n;
+            let c = 0;
+            let str = '';
+            while (r >= 0 && c < cols) {
+                if (r < rows) {
+                    str += horizontalJoin[r][c];
+                }
+                r -= 1;
+                c += 1;
+            }
+            diagArr.push(str);
+        }
+
+        for (let n = 0; n < cols + rows - 1; n++) {
+            let r = n;
+            let c = cols - 1;
+            let str = '';
+            while (r >= 0 && c >= 0) {
+                if (r < rows) {
+                    str += horizontalJoin[r][c];
+                }
+                r -= 1;
+                c -= 1;
+            }
+            diagArr.push(str);
+        }
+
+        for (let d of diagArr) {
+            if (d.includes(word) || d.includes(reverseWord)) {
                 return true;
             }
         }
@@ -30,6 +72,16 @@ const wordSearch = (letters, word) => {
     return false;
 };
 
-
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'R', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'N', 'E', 'A', 'R', 'G'],
+    ['W', 'H', 'L', 'S', 'Y', 'E', 'N', 'L'],
+    ['B', 'R', 'R', 'E', 'N', 'E', 'Y', 'K'],
+    ['F', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK'))
 
 module.exports = wordSearch
